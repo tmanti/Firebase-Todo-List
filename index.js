942f40f272c7281c29c60c916d8a91c5ac7ad01c
tmanti <tmanti17@gmail.com>
2018-05-23 15:27:38 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
+var uid = firebase.auth().currentUser.uid;
+var database = firebase.database()
+var user = firebase.auth().currentUser;
 
 function addToList(){
   var input = document.getElementById("addTo").value;
   if(input.length > 0){
-    document.getElementById("list").innerHTML += input + "<br />";
-    document.getElementById("addTo").value = ""; 
+   // document.getElementById("list").innerHTML += input + "<br />";
+    //document.getElementById("addTo").value = ""; 
+    writeNewPost
+    
+  }
+}
+
+function checkAuth(){
+  var user = firebase.auth().currentUser;
+  alert(Date.now());
+  
+}
+
+function writeNewPost(uid, username, time, message) {
+    // A post entry.
+    var postData = {
+      user: user,
+      uid: uid,
+      message: message,
+      time: time
+    };
+  
+    // Get a key for a new Post.
+    var newPostKey = firebase.database().ref().child('posts').push().key;
+  
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/user/' + uid + '/' + newPostKey] = postData;
+  
+    return firebase.database().ref().update(updates);
   }
 }
 
