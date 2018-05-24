9c514d76bf4a89f13d54c7e86d0ef5f9350cc6bc
tmanti <tmanti17@gmail.com>
2018-05-24 18:38:41 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 var uid;
 var database;
 var username;
+var data;
+const firestore = firebase.firestore();
+const settings = {timestampsInSnapshots: true};
+firestore.settings(settings);
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     uid = firebase.auth().currentUser.uid;
-    database = firebase.database()
-    username = firebase.auth().currentUser;
+    database = firebase.firestore()
+    username = firebase.auth().currentUser.email;
+    USER = user
+    DB = database
+    database.collection("users").doc(username).get().then(function(doc){
+      if(doc.exists){
+        data = doc.data();
+        DB = database.collection("users").doc(user.email);
+      } else{
+        init();
+      }
+    });
   } else {
-    window.assign.location("SignIn.html");
+    window.assign.location("index.html");
   }
 });
+
+function init(){
+  if(USER&&DB){
+    var progress = newUser();
+    DB.collection("users").doc(username).set(progress).then(function(doc){
+      loction.reload();
+    }).catch(function(error){
+      console.log(error.message);
+    });
+  }
+}
+
+function newUser(){
+  return {
+    "todo": []
+  }
+}
 
 function addToList(){
   var input = document.getElementById("addTo").value;
   if(input.length > 0){
-   // document.getElementById("list").innerHTML += input + "<br />";
-    //document.getElementById("addTo").value = ""; 
-    writeNewPost(uid, username, Date.now(), input);
-    
+   //input + "<br />";
+    data["todo"].push(input)
+    database.collection("users").doc(username).set(data).then(function(){
+      console.log("Written!")
+    });
+    document.getElementById("addTo").value = "";
   }
 }
 
@@ -26,25 +59,6 @@ function checkAuth(){
   var user = firebase.auth().currentUser;
   alert(uid);
   
-}
-
-function writeNewPost(uid, username, time, message) {
-    // A post entry.
-    var postData = {
-      user: username,
-      uid: uid,
-      message: message,
-      time: time
-    };
-  
-    // Get a key for a new Post.
-    var newPostKey = firebase.database().ref().child('posts').push().key;
-  
-    // Write the new post's data simultaneously in the posts list and the user's post list.
-    var updates = {};
-    updates['/user/' + uid + '/' + newPostKey] = postData;
-  
-    return firebase.database().ref().update(updates);
 }
 
 function searchKeyPress(e)
