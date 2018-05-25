eb6f3068bce19a69e5e1b05fc34d23676c8297ef
tmanti <tmanti17@gmail.com>
2018-05-25 13:31:13 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var uid;
 var database;
-var username;
 var data;
+var username;
 const firestore = firebase.firestore();
 const settings = {timestampsInSnapshots: true};
 firestore.settings(settings);
+
+console.log(firebase.auth().currentUser);
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
@@ -13,10 +15,18 @@ firebase.auth().onAuthStateChanged(function(user) {
     username = firebase.auth().currentUser.email;
     USER = user
     DB = database
+    firestore.collection("users").doc(firebase.auth().currentUser.email).onSnapshot(function(doc) {
+      console.log("check")
+    });
     database.collection("users").doc(username).get().then(function(doc){
       if(doc.exists){
         data = doc.data();
         DB = database.collection("users").doc(user.email);
+        if(data["todo"].length > 0){
+          for(i = 0; i<data["todo"].length; i++){
+            document.getElementById("list").innerHTML += data["todo"][i] + "<button id=" + i + " style='float: right;' onClick='remove(" + i + ")'>X</button>" + "</br></br>"
+          }
+        }
       } else{
         init();
       }
@@ -37,6 +47,21 @@ function init(){
   }
 }
 
+function remove(x){
+  var arr = data["todo"];
+  arr.splice(x, 1);
+  data["todo"] = arr;
+  database.collection("users").doc(username).set(data).then(function(){
+    console.log("Written!")
+  });
+  document.getElementById("list").innerHTML = ""
+  if(data["todo"].length > 0){
+    for(i = 0; i<data["todo"].length; i++){
+      document.getElementById("list").innerHTML += data["todo"][i] + "<button id=" + i + " style='float: right;' onClick='remove(" + i + ")'>X</button>" + "</br></br>"
+    }
+  }
+}
+
 function newUser(){
   return {
     "todo": []
@@ -52,6 +77,12 @@ function addToList(){
       console.log("Written!")
     });
     document.getElementById("addTo").value = "";
+    document.getElementById("list").innerHTML = ""
+    if(data["todo"].length > 0){
+      for(i = 0; i<data["todo"].length; i++){
+        document.getElementById("list").innerHTML += data["todo"][i] + "<button id=" + i + " style='float: right;' onClick='remove(" + i + ")'>X</button>" + "</br></br>"
+      }
+    }
   }
 }
 
