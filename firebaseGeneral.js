34601fbc020fdc1b39443c213a3ff6e212b3661e
tmanti <tmanti17@gmail.com>
2018-06-13 08:25:05 -0400

    Add files via upload

--- firebaseGeneral.js
+++ firebaseGeneral.js
@@ -1,2 +1,10 @@
 // Initialize Firebase
-//dont upload this dum dum
+var config = {
+  apiKey: "AIzaSyDSYnkgjJaddCmsFcMZiRYfjj6AEgfKA1g",
+  authDomain: "to-do-list-test-3d045.firebaseapp.com",
+  databaseURL: "https://to-do-list-test-3d045.firebaseio.com",
+  projectId: "to-do-list-test-3d045",
+  storageBucket: "to-do-list-test-3d045.appspot.com",
+  messagingSenderId: "774312091344"
+};
+firebase.initializeApp(config);
